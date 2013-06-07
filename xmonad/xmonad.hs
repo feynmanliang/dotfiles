ee2e18804950056671df9e96eb94883d74b86601
Feynman Liang <feynman.liang@gmail.com>
2013-06-07 10:28:57 -0700

    Updated for laptop

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -54,12 +54,12 @@ myBitmapsDir = "/home/fliang/.xmonad/dzen2"
 modMask' :: KeyMask
 
 -- 1080p external monitor settings
-modMask' = mod4Mask -- WinKey
-myStatusBar = "conky -c /home/fliang/.xmonad/.conky_dzen | dzen2 -x '1030' -w '718' -h '24' -ta 'r' -bg '#1B1D1E' -fg '#FFFFFF' -y '0'" -- for 1080p
+-- modMask' = mod4Mask -- WinKey
+-- myStatusBar = "conky -c /home/fliang/.xmonad/.conky_dzen | dzen2 -x '1030' -w '718' -h '24' -ta 'r' -bg '#1B1D1E' -fg '#FFFFFF' -y '0'" -- for 1080p
 
 -- 1600x900 native laptop settings
--- modMask' = mod1Mask -- Alt
--- myStatusBar = "conky -c /home/fliang/.xmonad/.conky_dzen | dzen2 -x '750' -w '718' -h '24' -ta 'r' -bg '#1B1D1E' -fg '#FFFFFF' -y '0'" -- for 1600x900
+modMask' = mod1Mask -- Alt
+myStatusBar = "conky -c /home/fliang/.xmonad/.conky_dzen | dzen2 -x '750' -w '718' -h '24' -ta 'r' -bg '#1B1D1E' -fg '#FFFFFF' -y '0'" -- for 1600x900
 
 --}}}
 -- Main {{{
