5cd02b1cef7500739cee507a3abc0f0a5f90518d
Feynman Liang <feynman.liang@gmail.com>
2014-03-22 12:21:29 -0700

    Update submodules and add symlink script

---
+++ make_symlinks.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+FILES=" ackrc
+aliases
+colors
+gitconfig
+gitignore_global
+gvimrc
+gvimrc.after
+inputrc
+janus
+tmux.conf
+vim
+vimrc
+vimrc.after
+vimrc.before
+Xresources
+zlogin
+zlogout
+zprezto
+zpreztorc
+zprofile
+zshenv
+zshrc"
+for f in $FILES
+do
+  echo "Is ~/.$f exists"
+  if [[ -L ~/.$f ]]
+  then
+    read -p "Conflict detected for ~/.$f, replace (y/n)?"
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      echo "Removing ~/.$f"
+      rm ~/.$f
+      echo "Symlinking $f -> ~/dotfiles/$f"
+      ln -s ~/dotfiles/$f ~/.$f
+    fi
+  else
+    echo "Symlinking $f..."
+    ln -s ~/dotfiles/$f ~/.$f
+  fi
+done
